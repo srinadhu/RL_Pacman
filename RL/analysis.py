ff99ecca1caa5039e4cc1cf0a8ba98a72cd153a0
srinadhu <2015csb1015@iitrpr.ac.in>
2018-04-30 17:06:37 +0530

    added code for q2

--- RL/analysis.py
+++ RL/analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -21,7 +21,7 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    answerNoise = 0.00125
     return answerDiscount, answerNoise
 
 def question3a():
